d79ca0dd32d2f384594caaba02f930b8f74fd7ec
Yaroslava Nechaieva <shdwrazedev@gmail.com>
2026-05-17 22:11:54 +0200

    feat: increase WebSocket max frame size to 5 MB for relay frame acceptance

--- holypunch-server/src/main/java/io/github/sever0x/holypunch/server/config/SignalingWebSocketConfig.java
+++ holypunch-server/src/main/java/io/github/sever0x/holypunch/server/config/SignalingWebSocketConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
+import reactor.netty.http.server.WebsocketServerSpec;
 
 import java.util.Map;
 
 @Configuration
 public class SignalingWebSocketConfig {
+
+    // CHUNK_SIZE (4 MB) + encryption overhead (IV + type + GCM tag) = ~4 MB per frame.
+    // Netty defaults to 64 KB; raise to 5 MB so relay frames are accepted.
+    private static final int MAX_FRAME_BYTES = 5 * 1024 * 1024;
 
     @Bean
     public HandlerMapping webSocketHandlerMapping(SignalingWebSocketHandler handler) {
@@ -22,6 +29,8 @@ public class SignalingWebSocketConfig {
 
     @Bean
     public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter();
+        var strategy = new ReactorNettyRequestUpgradeStrategy(
+                () -> WebsocketServerSpec.builder().maxFramePayloadLength(MAX_FRAME_BYTES));
+        return new WebSocketHandlerAdapter(new HandshakeWebSocketService(strategy));
     }
 }
